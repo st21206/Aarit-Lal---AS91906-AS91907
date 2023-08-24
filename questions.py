afade34007c768739d050d57504c90560f676fa2
Aarit <st21206@ormiston.school.nz>
2023-08-24 13:32:50 +1200

    Questions File Finished

--- questions.py
+++ questions.py
@@ -27,6 +27,97 @@ alternatives    = ['Earth, Mars, Pluto']
 
 
 [[space.questions]]
-question        = 'What is the smallest planet within our solar system?'
-answers         = ['Mercury']
-alternatives    = ['Earth, Neptune, Venus']
+question        = 'Which planet is known as the Morning Star?'
+answers         = ['Venus']
+alternatives    = ['Earth, Mars, Saturn']
+
+
+[[space.questions]]
+question        = 'Which planet is known as the Evening Star?'
+answers         = ['Venus']
+alternatives    = ['Pluto, Neptune, Uranus']
+
+
+[[space.questions]]
+question        = 'What is the oldest planet within our solar system?'
+answers         = ['Jupiter']
+alternatives    = ['Uranus, Neptune, Venus']
+
+
+[[space.questions]]
+question        = 'Which planet has the fastest rotation?'
+answers         = ['Jupiter']
+alternatives    = ['Earth, Mercury, Mars']
+
+
+[[space.questions]]
+question        = 'Which planets have no moons?'
+answers         = ['Mercury and Venus']
+alternatives    = ['Earth and Neptune, Jupiter and Pluto, Uranus and Saturn']
+
+
+[[space.questions]]
+question        = 'How long is one year on Jupiter?'
+answers         = ['12 Earth Years']
+alternatives    = ['28 Earth Years, 16 Moon Years, 2.5 Earth Years']
+
+
+[[space.questions]]
+question        = 'Which planet spins backward relative to the others?'
+answers         = ['Venus']
+alternatives    = ['Uranus, Mercury, Pluto']
+
+
+[[space.questions]]
+question        = 'In what year did Pluto become reclassified as a dwarf planet?'
+answers         = ['2006']
+alternatives    = ['2005, 2012, 1998']
+
+
+[[space.questions]]
+question        = 'Which planet rotates on its side?'
+answers         = ['Uranus']
+alternatives    = ['Pluto, Neptune, Venus']
+
+
+[[space.questions]]
+question        = 'Which planet has supersonic winds?'
+answers         = ['Neptune']
+alternatives    = ['Mercury, Uranus, Saturn']
+
+
+[[space.questions]]
+question        = 'What is the second smallest planet within our solar system?'
+answers         = ['Mars']
+alternatives    = ['Earth, Pluto, Saturn']
+
+
+[[space.questions]]
+question        = 'Uranus has only been visited by what spacecraft?'
+answers         = ['The Voyager 2']
+alternatives    = ['The undercarrier 1, Voyager 16, The Millenium Falcon']
+
+
+[[space.questions]]
+question        = 'What protects Earth from Meteroids and Radiation from the Sun?'
+answers         = ['Its atmosphere']
+alternatives    = ['Aliens, The International Space Station, Nothing']
+
+
+[[space.questions]]
+question        = 'How old is the universe?'
+answers         = ['13.7 Billion Years Old']
+alternatives    = ['66.5 Million Years Old, 99.8 Billion Years Old, Unknown']
+
+
+[[space.questions]]
+question        = 'Which planet has the largest ocean?'
+answers         = ['Jupiter']
+alternatives    = ['Earth, Mars, Pluto']
+
+
+[[space.questions]]
+question        = 'What do you call a large group of stars, dust and gas?'
+answers         = ['Galaxy']
+alternatives    = ['Sandbox, Black Hole, Universe']
+
