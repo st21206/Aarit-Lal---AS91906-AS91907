78372e376b69a03183ff447bd1661cbffb622f1b
Aarit <st21206@ormiston.school.nz>
2023-08-24 12:34:23 +1200

    Couple of space questions done

---
+++ questions.py
@@ -0,0 +1,32 @@
+[space]
+label = 'Space'
+
+
+[[space.questions]]
+question        = 'What is the smallest planet within our solar system?'
+answer          = ['Mercury']
+alternatives    = ['Earth, Neptune, Venus']
+
+
+[[space.questions]]
+question        = 'The moon called Titan orbits which planet?'
+answer          = ['Saturn']
+alternatives    = ['Mercury, Neptune, Uranus']
+
+
+[[space.questions]]
+question        = 'There have been more missions to this planet versus any other planet'
+answer          = ['Mars']
+alternatives    = ['Venus, Saturn, Jupiter']
+
+
+[[space.questions]]
+question        = 'Which planet has the most volcanoes?'
+answers         = ['Venus']
+alternatives    = ['Earth, Mars, Pluto']
+
+
+[[space.questions]]
+question        = 'What is the smallest planet within our solar system?'
+answers         = ['Mercury']
+alternatives    = ['Earth, Neptune, Venus']
