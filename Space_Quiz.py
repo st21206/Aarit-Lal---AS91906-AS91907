2b437aaea4e15421e7566d13239213f6c59c5616
Aarit <st21206@ormiston.school.nz>
2023-10-09 09:36:21 +1300

    Defining Image and Creating Labels

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -1,1 +1,12 @@
-import tkinter as tk
+from tkinter import *
+
+root = Tk()
+root.title("Aarit's Space Quiz")
+root.geometry('800x500')
+
+#Defining the image
+bg = PhotoImage(file="images/Space-Background-Image.jpg")
+
+#Create a label
+my_label = Label(root, image=bg)
+my_label.place(x=0, y=0, redwidth=1, relheight=1)
