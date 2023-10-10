e64d0225a6287f1ff0fe1c0b9fb466f6a2014fa7
Aarit <st21206@ormiston.school.nz>
2023-10-10 14:06:14 +1300

    Created Canvas, beginning resizer

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -1,12 +1,21 @@
 from tkinter import *
-
+from PIL import ImageTK, Image
 root = Tk()
 root.title("Aarit's Space Quiz")
-root.geometry('800x500')
+root.geometry("800x500")
 
+#Background code below
 #Defining the image
-bg = PhotoImage(file="images/Space-Background-Image.jpg")
+bg = PhotoImage(file="images/Space_Background_Image.png")
 
-#Create a label
-my_label = Label(root, image=bg)
-my_label.place(x=0, y=0, redwidth=1, relheight=1)
+#Create a canvas
+my_canvas = Canvas(root, width=800, height=500)
+my_canvas.pack(fill='both', expand=True)
+
+#Set image in canvas
+my_canvas.create_image(0,0, image=bg, anchor='nw')
+
+#Resizer
+
+root.bind('<Configure>', resizer)
+root.mainloop()
