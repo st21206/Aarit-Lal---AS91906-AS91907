48b02e573ae356fac457ca74d859aa3ced88dca3
Aarit <st21206@ormiston.school.nz>
2023-10-10 14:12:02 +1300

    Defined the resizer, now resizing image

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -6,7 +6,7 @@ root.geometry("800x500")
 
 #Background code below
 #Defining the image
-bg = PhotoImage(file="images/Space_Background_Image.png")
+bg = ImageTk.PhotoImage(file="images/Space_Background_Image.png")
 
 #Create a canvas
 my_canvas = Canvas(root, width=800, height=500)
@@ -16,6 +16,9 @@ my_canvas.pack(fill='both', expand=True)
 my_canvas.create_image(0,0, image=bg, anchor='nw')
 
 #Resizer
+def resizer(e):
+    Space_Background = Image.open('images/Space_Background_Image.pn')
+    resized_Space_Background = Space_Background.resize((e.width, e.height), Image.ANTIALIAS)
 
 root.bind('<Configure>', resizer)
 root.mainloop()
