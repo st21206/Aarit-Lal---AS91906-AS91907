dba48d0ccc40ae996dde778ee0ec8dd9a58acfa6
Aarit <st21206@ormiston.school.nz>
2023-10-10 14:58:10 +1300

    test

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -1,30 +1,37 @@
 from tkinter import *
 from PIL import ImageTk, Image
-root = Tk()
-root.title("Aarit's Space Quiz")
-root.geometry("800x500")
 
-#Background code below
-#Defining the image
-bg = ImageTk.PhotoImage(file="images/Space_Background_Image.png")
+class SpaceQuiz:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Aarit's Space Quiz")
+        self.root.geometry("800x500")
+        
+        # Load the original background image
+        self.original_bg = Image.open('images/Space_Background_Image.png')
+        self.bg = ImageTk.PhotoImage(self.original_bg)
 
-#Create a canvas
-my_canvas = Canvas(root, width=800, height=500)
-my_canvas.pack(fill='both', expand=True)
+        # Create a label to display the image without resizing
+        self.bg_label = Label(root, image=self.bg)
+        self.bg_label.pack()
+        
+        # Bind the resize function to window resize event
+        self.root.bind('<Configure>', self.resizer)
+        
+    def resize_image(self, event):
+        aspect_ratio = self.original_bg.width / self.original_bg.height
+        new_width = event.width
+        new_height = int(new_width / aspect_ratio)
 
-#Set image in canvas
-my_canvas.create_image(0,0, image=bg, anchor='nw')
+    def resizer(self, event):
+        # Get the resized image
+        resized_image = self.resize_image(event)
+        # Update the label with the resized image
+        self.bg_label.configure(image=resized_image)
+        self.bg_label.image = resized_image  # keep a reference to the image object to prevent garbage collection
 
-#Resizer
-def resizer(e):
-    global Space_Background, resized_bg, new_bg 
-    Space_Background = Image.open('images/Space_Background_Image.png')
-    resized_bg = Space_Background.resize((e.width, e.height), Image.ANTIALIAS)
-    new_bg = ImageTk.PhotoImage(resized_bg)
-    my_canvas.create_image(0,0, image=new_bg, anchor='nw')
-    my_canvas.itemconfig(bg, image=new_bg)
-    #make sure to add text below
 
-    
-root.bind('<Configure>', resizer)
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    app = SpaceQuiz(root)
+    root.mainloop()
