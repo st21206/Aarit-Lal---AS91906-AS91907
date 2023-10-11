f6138705c1c050c535469101495f69c63aedce06
Aarit <st21206@ormiston.school.nz>
2023-10-12 12:59:36 +1300

    code is working as intended, part 1 of implementing texzt

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -1,31 +1,34 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from datetime import datetime
+from tkinter import messagebox
 
 
 #make a window
-ws = Tk()
-ws.title("window")
+root = Tk()
+root.title("window")
 
 #get wigth & height of screen
-width= ws.winfo_screenwidth()
-height= ws.winfo_screenheight()
+width= root.winfo_screenwidth()
+height= root.winfo_screenheight()
 
 #set screensize as fullscreen and not resizable
-ws.geometry("%dx%d" % (width, height))
-ws.resizable(True, True)
+root.geometry("%dx%d" % (width, height))
+root.resizable(True, True)
 
 # put image in a label and place label as background
 imgTemp = Image.open("/Users/admiralaarit/Desktop/Space_Background_Image.png")
 img2 = imgTemp.resize((width,height))
 img = ImageTk.PhotoImage(img2)
 
-label = Label(ws,image=img)
+label = Label(root,image=img)
 label.pack(side='top',fill=Y,expand=True)
 
-#text = Text(ws,height=10,width=53)
-#text.place(x=30, y=50)
+#text, welcome and boundary testing through age, ages 0-8 not allowed
+def age():
+    pass
 
-#button = Button(ws,text='SEND',relief=RAISED,font=('Arial Bold', 18))
-#button.place(x=190, y=250)
 
-ws.mainloop()
+
+
+root.mainloop()
