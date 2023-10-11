778c30e4a8553b3ffa0d60796ec467d637aa083a
Aarit <st21206@ormiston.school.nz>
2023-10-12 12:03:29 +1300

    Changed entire code to implement a background image, wasn't working before

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -1,37 +1,31 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-class SpaceQuiz:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Aarit's Space Quiz")
-        self.root.geometry("800x500")
-        
-        # Load the original background image
-        self.original_bg = Image.open('images/Space_Background_Image.png')
-        self.bg = ImageTk.PhotoImage(self.original_bg)
 
-        # Create a label to display the image without resizing
-        self.bg_label = Label(root, image=self.bg)
-        self.bg_label.pack()
-        
-        # Bind the resize function to window resize event
-        self.root.bind('<Configure>', self.resizer)
-        
-    def resize_image(self, event):
-        aspect_ratio = self.original_bg.width / self.original_bg.height
-        new_width = event.width
-        new_height = int(new_width / aspect_ratio)
+#make a window
+ws = Tk()
+ws.title("window")
 
-    def resizer(self, event):
-        # Get the resized image
-        resized_image = self.resize_image(event)
-        # Update the label with the resized image
-        self.bg_label.configure(image=resized_image)
-        self.bg_label.image = resized_image  # keep a reference to the image object to prevent garbage collection
+#get wigth & height of screen
+width= ws.winfo_screenwidth()
+height= ws.winfo_screenheight()
 
+#set screensize as fullscreen and not resizable
+ws.geometry("%dx%d" % (width, height))
+ws.resizable(True, True)
 
-if __name__ == "__main__":
-    root = Tk()
-    app = SpaceQuiz(root)
-    root.mainloop()
+# put image in a label and place label as background
+imgTemp = Image.open("/Users/admiralaarit/Desktop/Space_Background_Image.png")
+img2 = imgTemp.resize((width,height))
+img = ImageTk.PhotoImage(img2)
+
+label = Label(ws,image=img)
+label.pack(side='top',fill=Y,expand=True)
+
+#text = Text(ws,height=10,width=53)
+#text.place(x=30, y=50)
+
+#button = Button(ws,text='SEND',relief=RAISED,font=('Arial Bold', 18))
+#button.place(x=190, y=250)
+
+ws.mainloop()
