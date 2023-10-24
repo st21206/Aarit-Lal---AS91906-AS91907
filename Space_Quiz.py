95846fdfceba2b48c394b72c46a67db979711170
Aarit <st21206@ormiston.school.nz>
2023-10-25 03:42:21 +1300

    Adding class and initialzing the root window, going to start adding comments from now on

--- Space_Quiz.py
+++ Space_Quiz.py
@@ -4,3 +4,11 @@ import json
 from PIL import ImageTk, Image
 import random
 
+#Define a class named SpaceQuiz
+class SpaceQuiz:
+    #Constructor method to initialize the class
+    def __init__(self, root):
+        #Initialize the root window
+        self.root = root
+        self.root.title("Aarit's Space Quiz")  #Set the title of the window
+        self.root.attributes('-fullscreen', True)  #Set the window to fullscreen
